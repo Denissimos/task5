8770516856e52a4d2377c3c19a24d58cfba850d1
Denissimos <Dragon.ru12@mail.ru>
2016-06-24 21:43:56 +0600

    Create struct Roots and equation solving function

---
+++ D.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+Roots Discriminant(float a, float b, float c)
+{
+    Roots answer;
+    answer.D = (b * b) - (4 * a * c);
+    if (answer.D >= 0){
+	answer.x1 = (-b + sqrt(answer.D))/ (2 * a);
+        answer.x2 = (-b - sqrt(answer.D))/ (2 * a);
+    }
+    return answer;
+}
---
+++ D.h
@@ -0,0 +1,12 @@
+#ifndef d_h
+#define d_h
+
+typedef struct {
+    float x1;
+    float x2;
+    float D;
+} Roots;
+
+Roots Discriminant(float a, float b, float c);
+
+#endif
